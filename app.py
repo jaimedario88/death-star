f8cd32b9265d772119d6a437ed18ba69c3b2e8db
jaimedario88 <jaimedario88@gmail.com>
2021-06-26 11:38:41 -0500

    Bug on return on parse_data function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import json
+
 app = Flask(__name__)
 
 
@@ -7,28 +8,21 @@ db = []
 
 
 def parse_data(raw_data: str):
-    split_data = raw_data.split(',')
+    split_data = raw_data.split(",")
 
     for i, data in enumerate(split_data):
         if i == 0:
             timestamp = data
         else:
-            data = data.replace('(','').replace(')','')
-            data = data.split('|')
+            data = data.replace("(", "").replace(")", "")
+            data = data.split("|")
             ship_type = data[0]
-            mbm_measure = data [1]
+            mbm_measure = data[1]
 
-        return {
-            'timestamp': timestamp,
-            'ship_type': ship_type,
-            'mbm': mbm_measure
-        }
-    
-            
+    return {"timestamp": timestamp, "ship_type": ship_type, "mbm": mbm_measure}
 
 
-
-@app.route("/listen", methods=['POST'])
+@app.route("/listen", methods=["POST"])
 def listen():
     raw_data = request.get_data(as_text=True)
 
@@ -36,7 +30,7 @@ def listen():
 
     db.append(sensor_data)
 
-    with open('file.txt', 'a') as my_file:
+    with open("file.txt", "a") as my_file:
         my_file.write(raw_data)
-        my_file.write('\n')
+        my_file.write("\n")
     return "OK!"
